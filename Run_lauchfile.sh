52c0910fdf431940d6246299bdf1b50e08284d74
bonato47 <tristan_bonato@hotmail.com>
2023-03-28 17:44:50 +0200

    Update Run_lauchfile.sh

--- Run_lauchfile.sh
+++ Run_lauchfile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gnome-terminal
+gnome-terminal --tab -e "sh -c 'roslaunch opti_track_ros_interface vrpn_optitrack.launch'" 
+gnome-terminal --tab -e "sh -c ' roslaunch xsens_mti_driver xsens_mti_node.launch '" 
