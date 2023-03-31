700065aaa265f4d9bf51a3565e83e12325ac80ed
bonato47 <tristan_bonato@hotmail.com>
2023-03-31 11:12:16 +0200

    launch shotcret

--- Run_lauchfile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --tab -e "sh -c 'roslaunch opti_track_ros_interface vrpn_optitrack.launch'" 
-gnome-terminal --tab -e "sh -c ' roslaunch xsens_mti_driver xsens_mti_node.launch '" 
